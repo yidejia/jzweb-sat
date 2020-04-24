d5ab301dd94c250e79012939b55a9bd4db753ca8
长歌 <1282350001@qq.com>
2020-04-24 15:47:25 +0800

    [ * ] 日志优化

--- ccbpay.com.cn/src/Lib/HttpRequest.php
+++ ccbpay.com.cn/src/Lib/HttpRequest.php
@@ -214,7 +214,7 @@ class  HttpRequest
                 $log .= "======Log Start:" . date("Y-m-d H:i:s") . "======\n";
                 $log .= "请求的路由地址:" . $this->config['url_query'] . "\n";
                 $log .= "打印请求参数串:" . print_r($data, true) . "\n";
-                $log .= "签名后的串:" . $message . "\n";
+                // $log .= "签名后的串:" . $message . "\n";
                 $log .= "打印调试信息:" . sprintf("请求流水号:%s API:%s 响应状态码:%d", $data['tradeNo'], $trxCode, $res->getStatusCode()) . "\n";
                 $log .= "======Log End:" . date("Y-m-d H:i:s") . "=====\n";
                 @file_put_contents($this->config['log_file_path'], $log . "\n", FILE_APPEND);
@@ -230,7 +230,7 @@ class  HttpRequest
                 $log .= "======Error Start:" . date("Y-m-d H:i:s") . "======\n";
                 $log .= "请求的路由地址:" . $this->config['url_query'] . "\n";
                 $log .= "打印请求参数串:" . print_r($data, true) . "\n";
-                $log .= "签名后的串:" . $message . "\n";
+                // $log .= "签名后的串:" . $message . "\n";
                 $log .= "打印调试信息:" . sprintf("请求流水号:%s API:%s", $data['tradeNo'], $trxCode) . "\n";
                 $log .= "异常错误信息:" . $e->getMessage() . "\n";
                 $log .= "======Error End:" . date("Y-m-d H:i:s") . "======\n";
@@ -378,7 +378,7 @@ class  HttpRequest
             $log .= "交易代码:" . $trxCode . "\n";
             $log .= "请求的路由地址:" . $this->config['h5_url'] . "\n";
             $log .= "打印请求参数串:" . print_r($data, true) . "\n";
-            $log .= "签名后的串:" . print_r($message, true) . "\n";
+            // $log .= "签名后的串:" . print_r($message, true) . "\n";
             $log .= "======Log End:" . date("Y-m-d H:i:s") . "=====\n";
             @file_put_contents($this->config['log_file_path'], $log . "\n", FILE_APPEND);
         }
